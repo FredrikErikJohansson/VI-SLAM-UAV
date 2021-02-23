81cd7e90228c3fb0cf73d95b7c4acc3e884191a4
samuelllsvensson <samuel.svensson.95@gmail.com>
2021-02-23 12:49:51 +0100

    Remove nanoseconds from timestamp

--- utils/convert.py
+++ utils/convert.py
@@ -4,8 +4,9 @@ format_string = "%Y-%m-%d %H:%M:%S,%f"
 with open("dates.txt", "r") as f, open("timestamps.txt", "w") as f1:
     for line in f:
         stripped_line = line.strip()
+        truncated_line = stripped_line[:-4] # remove nanoseconds
         try:
-            dt_obj = datetime.strptime(stripped_line, format_string)
+            dt_obj = datetime.strptime(truncated_line, format_string)
             millisec = int(dt_obj.timestamp() * 1000)
             f1.write(str(millisec)+"\n")
         except: 
